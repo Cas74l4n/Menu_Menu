2189e2b2864d3c0e47a1d08ed5f65e9a8fb65ae4
caste <castelan.15@outlook.es>
2023-10-05 20:40:10 -0600

    Ercicio Terminado

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Menu_Opciones_Barra"
+include(":app")
+ 
